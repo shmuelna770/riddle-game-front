06c767b4e0212010350f47fe12f952717fc6c9be
shmuel nabul <shmuel9977@gmail.com>
2025-09-02 17:42:01 +0300

    build

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 export default function Login() {
   return (
     <div>
       login
-      <input type="text" placeholder="name" />
-      <input type="text" placeholder="password"/>
-      <button>login</button>
-      <Link to='/play'>game</Link>
+      <form action="submit">
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="password" />
+        <button type="submit">login</button>
+      </form>
+      <Link to="/play">game</Link>
     </div>
-  )
+  );
 }
