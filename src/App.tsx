55fab40422bd456471d09c50e2a914187e60add8
shmuel nabul <shmuel9977@gmail.com>
2025-09-03 09:10:08 +0300

    comment out Home route in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router";
 // import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Play from "./pages/Play";
 import SignUp from "./pages/SignUp";
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/play" element={<Play />} />
